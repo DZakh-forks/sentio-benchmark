b8d3156f5e59ea4f9ff50bedd4c970faea368e3d
Datapunk <87508558+0xDatapunk@users.noreply.github.com>
2025-04-10 18:02:55 -0400

    fix ponder

--- case_3_ethereum_block/ponder/ponder.schema.ts
+++ case_3_ethereum_block/ponder/ponder.schema.ts
@@ -1,6 +1,10 @@
 import { onchainTable } from "ponder";
 
-export const chainlinkPrice = onchainTable("chainlink_price", (t) => ({
-  timestamp: t.bigint().primaryKey(),
-  price: t.doublePrecision().notNull(),
+export const block = onchainTable("block", (t) => ({
+  id: t.varchar().primaryKey(),
+  number: t.bigint().notNull(),
+  hash: t.varchar().notNull(),
+  parentHash: t.varchar().notNull(),
+  timestamp: t.bigint().notNull(),
+  transactions: t.text().array(),
 }));
